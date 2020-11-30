737059faa1300b1998f89af8a00fb35e06b2730b
Manuel Gvirtz <manuel@MacBook-Pro-de-Manuel.local>
2020-11-30 11:14:20 -0300

    Corrected previous version

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,15 +1,15 @@
 // Do not change any of the function names
 
 function invocarCallback(cb) { 
-  // Invoca al callback `cb`
+  // Invoca al callback `cb`.
   cb();
 
   }
 
 function sumarArray(numeros, cb) {
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
+  // Suma todos los números enteros (int/integers) de un array ("numeros").
   // Pasa el resultado a `cb`. 'cb' sigue siendo un callback.
-  // No es necesario devolver nada
+  // No es necesario devolver nada.
 
   const sumaDeNumeros = numeros.reduce(function(a, b) {
     return a + b
@@ -29,10 +29,12 @@ function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  // Devuelve el nuevo array
   const arrayNuevo = []
   array.forEach(function(elemento2){
     arrayNuevo.push(cb(elemento2))
   });
+  return arrayNuevo
 }
 
 // No modificar nada debajo de esta línea
